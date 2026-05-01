527a8a630c631a9f773e09800c54779228cba6ef
infosabbir <infosabbir9@gmail.com>
2026-05-01 15:10:44 +0600

    some work with animatedTextComponent

--- lib/view/intro/components/animated_text_component.dart
+++ lib/view/intro/components/animated_text_component.dart
@@ -1,21 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:sabbir_portfolio/res/constants.dart';
 
 class AnimatedImageContainer extends StatefulWidget {
-  
   final double? width;
   final double? height;
-  
+
   const AnimatedImageContainer({super.key, this.width, this.height});
 
   @override
-  State<AnimatedImageContainer> createState() => _AnimatedImageContainerState();
+  State<AnimatedImageContainer> createState() =>
+      _AnimatedImageContainerState();
 }
 
-class _AnimatedImageContainerState extends State<AnimatedImageContainer> with SingleTickerProviderStateMixin {
-
+class _AnimatedImageContainerState extends State<AnimatedImageContainer>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        final value = _controller.value;
+        return Transform.translate(
+          offset: Offset(0, 2 * value),
+          child: Container(
+            height: widget.height,
+            width: widget.width,
+            padding: const EdgeInsets.all(defaultPadding / 4),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              gradient: const LinearGradient(
+                colors: [Colors.pinkAccent, Colors.blue],
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.pink,
+                  offset: Offset(-2, 0),
+                  blurRadius: 20,
+                ),
+                BoxShadow(
+                  color: Colors.blue,
+                  offset: Offset(2, 0),
+                  blurRadius: 20,
+                ),
+              ],
+            ),
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Image.asset('assets/images/image.png'),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
