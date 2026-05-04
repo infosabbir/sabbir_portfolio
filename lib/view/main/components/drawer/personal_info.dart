a81edb21ce3570b2434b9a7c576c503036a7f7f8
infosabbir <infosabbir9@gmail.com>
2026-05-04 06:52:22 +0600

    PersonalInfo done

--- lib/view/main/components/drawer/personal_info.dart
+++ lib/view/main/components/drawer/personal_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sabbir_portfolio/res/constants.dart';
+import 'package:sabbir_portfolio/view/main/components/drawer/header_info.dart';
 
 class PersonalInfo extends StatelessWidget {
   const PersonalInfo({super.key});
@@ -8,8 +9,14 @@ class PersonalInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [SizedBox(height: defaultPadding / 2),
-      
+      children: [
+        SizedBox(height: defaultPadding / 2),
+        HeaderInfo(title: 'Contact', text: '8801600998743'),
+        HeaderInfo(title: 'Email', text: 'sbbrinfo@gmail.com'),
+        HeaderInfo(title: 'Github', text: 'github.com/infosabbir'),
+        SizedBox(height: defaultPadding),
+        Text('Skills', style: TextStyle(color: Colors.white)),
+        SizedBox(height: defaultPadding),
       ],
     );
   }
