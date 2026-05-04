4ad50143cea5e29fe927406404fc7cb73b0b78be
infosabbir <infosabbir9@gmail.com>
2026-05-04 07:49:15 +0600

    contact_icon.dart done

--- lib/view/main/components/drawer/drawer.dart
+++ lib/view/main/components/drawer/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sabbir_portfolio/res/constants.dart';
 import 'package:sabbir_portfolio/view/main/components/drawer/about.dart';
+import 'package:sabbir_portfolio/view/main/components/drawer/knowleges.dart';
 import 'package:sabbir_portfolio/view/main/components/drawer/my_skills.dart';
 import 'package:sabbir_portfolio/view/main/components/drawer/personal_info.dart';
 
@@ -21,7 +22,13 @@ class CustomDrawer extends StatelessWidget {
                 padding: EdgeInsets.all(defaultPadding / 2),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [PersonalInfo(), MySkills(),],
+                  children: [
+                    PersonalInfo(),
+                    MySkills(),
+                    Knowleges(),
+                    Divider(),
+                    SizedBox(height: defaultPadding),
+                  ],
                 ),
               ),
             ),
